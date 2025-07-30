7d18606645f5ff19acb6080b89e34d4d283dc615
walaayahia3369 <walaayahia3369@gmail.com>
2025-07-30 11:40:02 +0300

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { Suspense } from "react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
